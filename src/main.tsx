0bcc260ac98379268ac3ce6791fc8f9b138ec1c2
Daniel Cazé <danielctlima@gmail.com>
2022-08-02 20:25:34 -0300

    organizing directories

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+//Create Root indica qual o elemento raiz da nossa pagina (sendo ele o id=root) e o render ele utiliza para renderirzar html dentro
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </StrictMode>
+);
